5a082970485174e2643584c61ba3edb12bc62e4a
PedroLedoBustos <pedroldesign@gmail.com>
2024-06-25 23:33:51 +0200

    Mensajes de ella

--- lib/presentacion/screens/chat/chat_screen.dart
+++ lib/presentacion/screens/chat/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mensajes_app/presentacion/widgets/chat/her_message_bubble.dart';
 import 'package:mensajes_app/presentacion/widgets/chat/my_message_bubble.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -36,7 +37,9 @@ class _ChatView extends StatelessWidget {
               child: ListView.builder(
                 itemCount: 101,
                 itemBuilder: (context, index) {
-                return const MyMessageBubble();
+                return (index %2==0)
+                ? const HerMessageBubble()
+                :const MyMessageBubble();
               },) 
             )],
           ),
